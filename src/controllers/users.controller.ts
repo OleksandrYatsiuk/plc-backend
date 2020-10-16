bb830416ed8ba58d7a6868128c657c23e14a1e54
Oleksandr Yatsiuk <oleksandr.yatsiuk@gmail.com>
2020-10-16 22:08:26 +0300

    added user routers

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -19,6 +19,8 @@ export class UsersController extends BaseController {
         this.router.get(`${this.path}`, this.getList);
         this.router.post(`${this.path}/register`, this.register);
         this.router.patch(`${this.path}/current`, this.update);
+        this.router.get(`${this.path}`, this.geItem);
+        this.router.delete(`${this.path}/:id`, this.removeItem);
     }
     // phone
     private register = (request: express.Request, response: express.Response, next: express.NextFunction): void => {
@@ -34,8 +36,41 @@ export class UsersController extends BaseController {
                         .then(user => response.status(200).json({ result: this.parseModel(user) }))
                 }
             })
-
     };
+
+
+    private update = (request: express.Request, response: express.Response, next: express.NextFunction): void => {
+        const data: User = request.body;
+        this.model.findOneAndUpdate({ phone: data.phone }, { ...data, updatedAt: Date.now() }, { new: true })
+            .then(user => response.status(200).json({ result: this.parseModel(user) }))
+            .catch(err => response.status(422).json({ result: err.message || err }));
+    }
+
+    private getList = (request: express.Request, response: express.Response, next: express.NextFunction): void => {
+        const data = request.body;
+        this.model.paginate({}, { page: data.page || 1, limit: data.limit || 20 })
+            .then(({ docs, total, limit, page, pages }) => {
+                response.status(200).json({ result: docs.map(user => this.parseModel(user)) });
+            })
+            .catch(err => response.status(422).json({ result: err.message || err }));
+    }
+
+    private geItem = (request: express.Request, response: express.Response, next: express.NextFunction): void => {
+        const data = request.params;
+        this.model.findOne(data)
+            .then(user => {
+                response.status(200).json({ result: this.parseModel(user) })
+            })
+            .catch(err => response.status(422).json({ result: err.message || err }));
+    }
+
+    private removeItem = (request: express.Request, response: express.Response, next: express.NextFunction): void => {
+        const { id } = request.query
+        this.model.findByIdAndDelete(id)
+            .then(user => response.status(204).json())
+            .catch(err => response.status(422).json({ result: err.message || err }));
+    }
+
     private parseModel(user: User) {
         return {
             id: user._id,
@@ -47,20 +82,5 @@ export class UsersController extends BaseController {
             createdAt: user.createdAt,
             updatedAt: user.updatedAt
         }
-    }
-
-    private update = (request: express.Request, response: express.Response, next: express.NextFunction): void => {
-        const data: User = request.body;
-        this.model.findOneAndUpdate({ phone: data.phone }, { ...data, updatedAt: Date.now() }, { new: true })
-            .then(user => response.status(200).json({ result: this.parseModel(user) }))
-            .catch(err => response.status(422).json({ result: err.message || err }));
-    }
-    private getList = (request: express.Request, response: express.Response, next: express.NextFunction): void => {
-        const data = request.body;
-        this.model.paginate({}, { page: data.page || 1, limit: data.limit || 20 })
-            .then(({ docs, total, limit, page, pages }) => {
-                response.status(200).json({ result: docs.map(user => this.parseModel(user)) });
-            })
-            .catch(err => response.status(422).json({ result: err.message || err }));
     }
 }
