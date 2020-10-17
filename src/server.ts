f7ec45464963d6073cc3cd8860e8a6b50f547ffd
Oleksandr Yatsiuk <oleksandr.yatsiuk@gmail.com>
2020-10-17 22:42:30 +0300

    added controllers

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import App from './app';
 import 'dotenv/config';
-import { PaymentsController, UsersController } from './controllers';
+import { CoursesController, LessonsController, PaymentsController, UsersController } from './controllers';
 
 const app = new App(
 	[
 		new PaymentsController(),
-		new UsersController()
+		new UsersController(),
+		new CoursesController(),
+		new LessonsController()
 	],
 	+process.env.PORT,
 	'/v1'
