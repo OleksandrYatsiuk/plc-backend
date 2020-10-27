80da9601ad25a889b140a240e9af7e4012b74acb
Oleksandr Yatsiuk <oleksandr.yatsiuk@gmail.com>
2020-10-27 07:26:51 +0200

    fix bot life

--- src/request.ts
+++ src/request.ts
@@ -3,6 +3,6 @@ export function sendRequest() {
     setInterval((() => {
         axios.get('https://lesson-backend.herokuapp.com/api/v1/courses')
         console.log('activate backend heroku ')
-    }), 0.45 * 36000)
+    }), 0.45 * 3600)
 
 }
