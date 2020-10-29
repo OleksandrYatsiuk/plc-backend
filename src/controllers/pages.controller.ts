948b3a4f97b1c466c4c6d365a9fd955ae2043082
Oleksandr Yatsiuk <oleksandr.yatsiuk@gmail.com>
2020-10-29 09:12:33 +0200

    update older static pages

--- src/controllers/pages.controller.ts
+++ src/controllers/pages.controller.ts
@@ -3,7 +3,7 @@ import * as mongoose from 'mongoose';
 import BaseController from './base.controller';
 import model from './schemas/pages.schema';
 import { IStaticPages } from '../interfaces/index'
-import {  UnprocessableEntityException } from '../exceptions/index';
+import { UnprocessableEntityException } from '../exceptions/index';
 
 export class PagesController extends BaseController {
     public path = '/static-pages';
@@ -21,16 +21,25 @@ export class PagesController extends BaseController {
     }
     private create = (request: express.Request, response: express.Response, next: express.NextFunction): void => {
         const data: IStaticPages = request.body;
-        this.model.create(data)
-            .then(user => response.status(200).json({ result: this.parseModel(user) }))
-            .catch(err => next(new UnprocessableEntityException([{ field: 'type', message: err.message }])))
+        this.model.exists({ type: data.type }).then(exist => {
+            if (exist) {
+                this.model.updateOne({ type: data.type }, data)
+                    .then(page => response.status(200).json({ result: this.parseModel(page) }))
+                    .catch(err => next(new UnprocessableEntityException([{ field: 'type', message: err.message }])))
+            } else {
+                this.model.create(data)
+                    .then(page => response.status(200).json({ result: this.parseModel(page) }))
+                    .catch(err => next(new UnprocessableEntityException([{ field: 'type', message: err.message }])))
+            }
+        })
+
     };
 
 
     private getList = (request: express.Request, response: express.Response, next: express.NextFunction): void => {
         const data = request.query;
         this.model.find(data)
-            .then((pages:IStaticPages[]) => response.status(200).json({ result: pages.map(page => this.parseModel(page))}))
+            .then((pages: IStaticPages[]) => response.status(200).json({ result: pages.map(page => this.parseModel(page)) }))
             .catch(err => next(new UnprocessableEntityException([{ field: 'name', message: err.message }])))
     }
 
